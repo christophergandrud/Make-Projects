424623dd064c82b62f19e8f48fe579f1699e7851
Christopher Gandrud <christopher.gandrud@gmail.com>
2012-09-17 16:57:49 +0900

    Seminar/Lecture Creator

---
+++ Rnw_Lecture/LectureCreator.sh
@@ -0,0 +1,24 @@
+##########
+# Shell script to create lectures or seminars
+# Christopher Gandrud
+# Updated 17 September 2012
+# Helpful information found in "The Linux Command Line: A Complete Introduction" (Shotts 2012)
+##########
+
+#!/bin/bash
+
+read -p "Do you want to make a Lecture or Seminar? -> " l
+read -p "Please enter the lecture number you want to compile -> " i
+
+    # Make a new directory in the Lecture Folder
+    mkdir /Lectures/$l$i
+    cd /Lectures/$l$i
+
+    # Copy in style files
+    cp -i /Lectures/Lecture1/beamerouterthemeStats.sty beamerouterthemeStats.sty
+    
+    cp -i /git_repositories/Introduction_to_Statistics_and_Data_Analysis_Yonsei/Lectures/Lecture1/beamerthemeStats.sty beamerthemeStats.sty 
+    
+    # Create new beamer slide
+    cp -i /git_repositories/Introduction_to_Statistics_and_Data_Analysis_Yonsei/Lectures/CommonFiles/Header.txt  $l$i.Rnw
+    
